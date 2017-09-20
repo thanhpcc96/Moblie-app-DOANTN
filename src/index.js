cfc7deccb6017cc3a2cef47748b303119f2a3d9b
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-20 21:09:27 +0700

    create app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    View, Text, StyleSheet
+} from 'react-native';
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={{ justifyContent: 'center', alignItems: 'center' }}> Do an tot nghiep</Text>
+
+            </View>
+        );
+    }
+}
+export default App;
