061fa9a1704a7b682265e0ecb7ef8dfeb4c158bc
Thanh Hao <haothanh@haothanhs-MacBook-Air.local>
2017-10-04 22:47:52 +0700

    nguyen cuu navigayion

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { MaterialIcons } from '@expo/vector-icons';
+import Touchable from '@appandflow/touchable';
+import styled from 'styled-components/native';
+
+import colors from '../../constains/color';
+
+cons
