28d2fd9ff56f7c7bc80b86259691ab6f8976b6e8
Thanh Pham <thanhpcc1996@gmail.com>
2017-10-05 17:57:31 +0700

    begin

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { MaterialIcons } from '@expo/vector-icons';
+import Touchable from '@appandflow/touchable';
+import styled from 'styled-components';
+
+import HomeNavigator from './HomeNavigator';
+
+import Colors from '../../constains/color';
+
+export default StackNavigator({
+    Home: { screen: HomeNavigator }
+});
